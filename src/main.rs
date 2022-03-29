b0579948c3c9202767da56aec7eba1642959db44
Mael Leclerc <leclerc.mael@protonmail.com>
2022-03-29 21:11:50 +0200

    feat: git push

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@ use std::process::{exit, Command};
 
 mod lib;
 
-fn git_commit(commit: CommitMsg) {
+fn git_add() {
     let add_output = Command::new("git").arg("add").arg(".").output();
     if add_output.is_err() {
         panic!("could not git add")
-    }
-    let _commit_output = Command::new("git")
+    };
+}
+fn git_commit(commit: CommitMsg) {
+    let commit_output = Command::new("git")
         .arg("commit")
         .arg("-m")
         .arg(commit.to_string())
         .output();
+    if commit_output.is_err() {
+        panic!("could not git commit")
+    };
 }
 
 fn get_type() -> CommitType {
     let mut rl = Editor::<()>::new();
     let commit_type: CommitType;
-    let read_type = rl.readline("commit type>> ");
+    let read_type = rl.readline("commit type=> ");
     match read_type {
         Ok(line) => {
             match line.as_str() {
@@ -53,7 +58,7 @@ fn get_type() -> CommitType {
 
 fn get_desc() -> String {
     let mut rl = Editor::<()>::new();
-    let read_description = rl.readline("description>> ");
+    let read_description = rl.readline("description=> ");
     match read_description {
         Ok(line) => line,
         Err(ReadlineError::Interrupted) => {
@@ -71,6 +76,39 @@ fn get_desc() -> String {
     }
 }
 
+fn push_y_or_n() -> bool {
+    let mut rl = Editor::<()>::new();
+    let read_type = rl.readline("commit type=> ");
+    match read_type {
+        Ok(line) => match line.as_str() {
+            "y" => true,
+            "n" => false,
+            _ => push_y_or_n(),
+        },
+        Err(ReadlineError::Interrupted) => {
+            eprintln!("CTRL-C");
+            exit(0);
+        }
+        Err(ReadlineError::Eof) => {
+            eprintln!("CTRL-D");
+            exit(0);
+        }
+        Err(err) => {
+            eprintln!("Error: {:?}", err);
+            exit(0);
+        }
+    }
+}
+
+fn git_push(do_b: bool) {
+    if do_b {
+        let push_output = Command::new("git").arg("push").output();
+        if push_output.is_err() {
+            panic!("could not git commit")
+        };
+    }
+}
+
 fn main() {
     // TODO: add a -h/--help
     println!(
@@ -83,5 +121,7 @@ fn main() {
     );
     // TODO: parse the commit body
     let msg = CommitMsg::new(get_type(), get_desc(), None);
+    git_add();
     git_commit(msg);
+    git_push(push_y_or_n());
 }
