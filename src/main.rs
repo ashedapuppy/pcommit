d5f8644eed5e132f05d70fdbd594e4fd92f41b30
Mael LECLERC <leclerc.mael@protonmail.com>
2022-03-31 05:42:04 +0200

    feat: use clap for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,27 @@
+use crate::lib::CommitMsg;
 use colored::*;
 
 mod git;
 mod input;
 mod lib;
 
-use crate::lib::CommitMsg;
+use clap::Parser;
 
-use getopts::Options;
-use std::env;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Disables git add
+    #[clap(short = 'a', long = "no-add")]
+    noadd: bool,
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
-    print!("{}", opts.usage(&brief));
+    /// Disables git push
+    #[clap(short = 'p', long = "no-push")]
+    nopush: bool,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let program = args[0].clone();
-
-    let mut opts = Options::new();
-    opts.optflag("h", "help", "print this help menu");
-    let matches = match opts.parse(&args[1..]) {
-        Ok(m) => m,
-        Err(f) => {
-            panic!("{}", f.to_string())
-        }
-    };
-    if matches.opt_present("h") {
-        print_usage(&program, opts);
-        return;
-    }
+    let args = Args::parse();
     // TODO: add a -h/--help and/or parse command line args
     println!(
         "commit types :\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}",
@@ -40,7 +32,11 @@ fn main() {
         "test".magenta(),
     );
     let msg = CommitMsg::new(input::get_type(), input::get_desc(), input::get_body());
-    git::add();
+    if !args.noadd {
+        git::add();
+    }
     git::commit(msg);
-    git::push();
+    if !args.nopush {
+        git::push();
+    }
 }
