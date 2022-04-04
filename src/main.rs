fdb5a9ced08789ff10061edfd87fc1ccecc22592
Mael LECLERC <leclerc.mael@protonmail.com>
2022-04-04 13:13:16 +0200

    feat: documentation added to main function
    simple doc comment

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ mod lib;
 use clap::Parser;
 use colored::*;
 
-/// git commit formatter
+// git commit formatter
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-struct Args {
+struct Arguments {
     /// Disable git add
     #[clap(short = 'a', long = "no-add")]
     no_add: bool,
@@ -19,7 +19,17 @@ struct Args {
 }
 
 fn main() {
-    let args = Args::parse();
+    //! pcommit generates a commit message from user input according to
+    //! [this](https://gist.github.com/qoomon/5dfcdf8eec66a051ecd85625518cfd13)
+    //! commit message convention, it then adds modified files, creates the commit
+    //! and calls git push
+    //!
+    //! # Usage:
+    //! ./pcommit [-a/--no-add] [-p/--no-push] [-h/--help] [-V/--version]
+    //!
+    #![warn(missing_docs)]
+    #![warn(missing_doc_code_examples)]
+    let args = Arguments::parse();
     println!(
         "commit types :\n\t{}\n\t{}\n\t{}\n\t{}\n\t{}",
         "feat".red(),
